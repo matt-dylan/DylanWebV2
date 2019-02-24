38b60d5260e3a7962d1b5e2fb9381f75165b6ee8
Dylan <dylpickle222@gmail.com>
2019-02-24 00:06:56 -0600

    BoilerPlate Complete

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  //const [count, setCount] = useState(1);
+
+  return (
+    <div>
+      <h1>SAMPLE APP</h1>
+    </div>
+  );
+};
+
+export default App;
